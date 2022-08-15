85c15d09f8e6a557577cd5b8151553cad2af5102
Robin Minervini <robin.minervini@gmail.com>
2022-08-15 13:05:44 +0200

    feat(1): connect to goingueko api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
 import type { NextPage } from 'next'
+import type { FC } from 'react'
 
+import useAxios from 'axios-hooks'
 import AppHead from '~/components/AppHead'
 import Page from '~/components/layout/Page'
 import AppBox from '~/ui/AppBox'
 import Heading from '~/ui/typography/Heading'
 import Text from '~/ui/typography/Text'
+
+const CoinGuekoData: FC = () => {
+  const [{ data, loading, error }, refetch] = useAxios({
+    baseURL: 'https://api.coingecko.com/api/v3/ping',
+  })
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          refetch()
+        }}
+      >
+        Refetcg
+      </button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  )
+}
 
 const Index: NextPage = () => (
   <Page>
@@ -12,6 +36,7 @@ const Index: NextPage = () => (
     <Heading as="h1">Index Page</Heading>
     <AppBox mt={2}>
       <Text>Hello from index page</Text>
+      <CoinGuekoData />
     </AppBox>
   </Page>
 )
